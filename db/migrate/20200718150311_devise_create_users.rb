2434c157f929e27e15a17e16752d5688e92f5d19
Ken Ishii <19k1e1n04@gmail.com>
2020-09-07 01:41:50 +0900

    マイグレーションファイルを修正

--- db/migrate/20200718150311_devise_create_users.rb
+++ db/migrate/20200718150311_devise_create_users.rb
@@ -9,6 +9,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :encrypted_password, null: false, default: ""
       t.string :image, default: ""
       t.string :rate
+      t.string :scud, default: ""
 
       ## Recoverable
       t.string   :reset_password_token
