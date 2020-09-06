93edf6e2f291706dfb9038126b198c5ffb3e9928
Ken Ishii <19k1e1n04@gmail.com>
2020-09-07 00:24:22 +0900

    rate修正

--- db/migrate/20200718150311_devise_create_users.rb
+++ db/migrate/20200718150311_devise_create_users.rb
@@ -8,6 +8,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
       t.string :image, default: ""
+      t.string :rate
 
       ## Recoverable
       t.string   :reset_password_token
