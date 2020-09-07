861beeb6590ca297e6aef5fb06763ced535c2017
Ken Ishii <19k1e1n04@gmail.com>
2020-09-07 16:28:55 +0900

    Release to Heroku

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -60,7 +60,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super(resource)
   # end
   def after_update_path_for(resource)
-    timeline_posts_path(resource)
+    timeline_posts_path
   end
 
 end
