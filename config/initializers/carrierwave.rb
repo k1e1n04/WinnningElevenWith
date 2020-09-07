f654f17954df81e0c3ec50df8348399f85c987ee
Ken Ishii <19k1e1n04@gmail.com>
2020-09-07 22:59:13 +0900

    Release to Heroku

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,19 +3,23 @@ require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
-if Rails.env.production?
-  CarrierWave.configure do |config|
+CarrierWave.configure do |config|
+  if Rails.env.production? || Rails.env.development?
+    config.storage :fog
     config.fog_provider = 'fog/aws'
+    config.fog_directory     =  ENV['winningelevenwith']
+    config.asset_host = 'https://s3.ap-northeast-1.amazonaws.com/winningelevenwith'
+    config.fog_public = false
     config.fog_credentials = {
       # Amazon S3用の設定
       provider: 'AWS',
-      region: ENV['ap-northeast-1'],
-      aws_access_key_id: ENV['AKIA4GIXPXKY4OIN5KUR'],
-      aws_secret_access_key: ENV['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'],
+      region: 'ap-northeast-1',
+      aws_access_key_id: 'AKIA4GIXPXKY4OIN5KUR',
+      aws_secret_access_key: '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',
     }
-    config.fog_directory     =  ENV['winningelevenwith']
-    config.asset_host = 'https://s3.ap-northeast-1.amazonaws.com/winningelevenwith'
-    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
+  else
+    config.storage :file
+    config.enable_processing = false if Rails.env.test?
   end
 
   # 日本語ファイル名の設定
