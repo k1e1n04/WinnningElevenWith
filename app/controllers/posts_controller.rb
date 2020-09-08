84dda6bcdd912f7dcb21891a15b36daf0c948da2
Ken Ishii <19k1e1n04@gmail.com>
2020-09-09 03:18:21 +0900

    Release to Heroku

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -37,7 +37,7 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     @post.user_id = current_user.id
     if @post.save
-      redirect_to :action => "index"
+      redirect_to :action => "timeline"
     else
       render :action => "new"
     end
